ed8dc8e800fd6834d1fe4e1a822b50daadfa414f
hamzalakhani <hamzalakhani10@gmail.com>
2017-10-06 15:42:38 -0400

    Successfully implemented Facebook Login

--- helpingHandV2/AppDelegate.swift
+++ helpingHandV2/AppDelegate.swift
@@ -23,12 +23,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
-
         
-        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationOpenURLOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationOpenURLOptionsKey.annotation])
         
         return handled
     }
+
         
     
     
